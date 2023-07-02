db00f62df6a9385ea84b062185cf528aa4fc37af
Maxim <maxl08@list.ru>
2023-07-02 13:02:14 +0300

    new

---
+++ 3/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = 12;
+double NumberB = 5;
+Console.WriteLine (numberA / NumberB);
+int numberC = 12;
+int numberD = 5;
+Console.WriteLine (numberC / numberD);
---
+++ 4/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 10, b = 2, c = 6, d = 8, e = 4, max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Max = ");
+Console.Write(max);
